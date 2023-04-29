ffec1a296537faac1364aed1db4b9d669a059ffc
unknown <jithinbp007@gmail.com>
2023-04-29 13:20:30 +0530

    parkinglot

--- src/main/java/com/driver/model/Spot.java
+++ src/main/java/com/driver/model/Spot.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class Spot {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private SpotType spotType;
