fb2956f385d75128d8563b622dbe8f59aa610d33
unknown <jithinbp007@gmail.com>
2023-04-29 13:04:17 +0530

    parkinglot

--- src/main/java/com/driver/controllers/ReservationController.java
+++ src/main/java/com/driver/controllers/ReservationController.java
@@ -19,7 +19,14 @@ public class ReservationController {
         //Note that the vehicle can only be parked in a spot having a type equal to or larger than given vehicle
         //If parkingLot is not found, user is not found, or no spot is available, throw "Cannot make reservation" exception.
 
-        return reservationService.reserveSpot(userId, parkingLotId, timeInHours, numberOfWheels);
+        try{
+            Reservation reservation = reservationService.reserveSpot(userId, parkingLotId, timeInHours, numberOfWheels);
+            return reservation;
+        }catch (Exception e){
+
+            throw new Exception(e.getMessage());
+        }
+
 
     }
 }
